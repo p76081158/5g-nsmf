dce8fa457569e26b31f0fa2589f4bde2908025b2
p76081158 <p76081158@gs.ncku.edu.tw>
2021-06-22 12:24:41 +0800

    temporary push

--- 5g-nsmf.go
+++ 5g-nsmf.go
@@ -1,155 +1,150 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"strings"
-	//"encoding/json"
-	//"io/ioutil"
+	// "fmt"
+	// "time"
+	// "strconv"
 
-	"github.com/p76081158/5g-nsmf/slice_binpack"
-	"github.com/p76081158/5g-nsmf/yamlparse"
+	// "github.com/p76081158/5g-nsmf/modules/nsrhandler"
+	"github.com/p76081158/5g-nsmf/modules/optimizer/slicebinpack"
+	// "github.com/p76081158/5g-nsmf/modules/optimizer/scheduler"
+	// "github.com/p76081158/5g-nsmf/modules/ueransim/gnb"
+	"github.com/p76081158/5g-nsmf/modules/ueransim/ue/generator"
+	// "github.com/p76081158/5g-nsmf/api/f5gnssmf"
 	"github.com/p76081158/free5gc-nssmf"
-	// nssmfyamlparse "github.com/p76081158/free5gc-nssmf/yamlparse"
 )
 
-// 1000 == 1000 milicore == 1 cpu
-// 60 == 60 seconds
+// struct alias
+type Packer = slicebinpack.Packer
+type Bin = slicebinpack.Bin
+type Slice = slicebinpack.Slice
+type Block = slicebinpack.Block
+type SliceDeploy = slicebinpack.SliceDeploy
+type DrawBlock = slicebinpack.DrawBlock
+type UeGenerator = generator.UeGenerator
+
+// struct var
+var request []Slice
+var ueGenerator []UeGenerator
+var bin Bin
+var access []Slice
+var reject []Slice
+var deploy_info []SliceDeploy
+var draw_info []DrawBlock
+var p Packer
+
+// environment var
+//   1000 == 1000 milicore == 1 cpu
+//   60 == 60 seconds
 var ResourceLimit = 1000
 var TimeWindowSize = 600
+var TimeWindowDelay = 100
 var CPUofUserPlane = 200
-var DeployTimeBias = 1.02
+var DeployTimeBias = 1.04
+var DrawScaleRatio = 10
 var gnb_ip_dictionary = map[string]string{"466-01-000000010": "192.168.72.51", "466-11-000000010": "192.168.72.53", "466-93-000000010": "192.168.72.55"}
-var gnb_ip_B_dictionary = map[string]string{"466-01-000000010": "201", "466-11-000000010": "202", "466-93-000000010": "203"}
+var gnb_ip_B_dictionary = map[string]string{"466-01-000000010": "200", "466-11-000000010": "201", "466-93-000000010": "202"}
+var RequestPattern = "300"
 
-type Packer = slice_binpack.Packer
-type Bin = slice_binpack.Bin
-type Slice = slice_binpack.Slice
-type Block = slice_binpack.Block
-type SliceDeploy = slice_binpack.SliceDeploy
-
-// apply slice to core network (by calling the nssmf of free5gc)
-func ApplySliceToCoreNetwork(snssai string, gnb_ip string, gnb_n3_ip_B string, ngci string, cpu int, core_function_cpu int, start int, duration int, end bool) {
-	time.Sleep(time.Duration(float64(start) * DeployTimeBias) * time.Second)
-	nssmf.ApplyNetworkSlice(snssai, gnb_ip, gnb_n3_ip_B, ngci, cpu, core_function_cpu)
-	if end {
-		time.Sleep(time.Duration(duration) * time.Second)
-		DeleteSliceFromCoreNetwork(snssai)
-	}
-}
-
-// delete slice from core network (by calling the nssmf of free5gc)
-func DeleteSliceFromCoreNetwork(snssai string) {
-	nssmf.DeleteNetworkSlice(snssai)
-}
-
-// modify resource and apply to core network (by calling the nssmf of free5gc)
-func SliceModifyServiceCPU(snssai string, ngci string, cpu int, start int, duration int, end bool) {
-	//nssmfyamlparse.ModifyCPU(snssai, "upf", cpu)
-	time.Sleep(time.Duration(start) * time.Second)
-	nssmf.ApplyServiceCpuChange(snssai, ngci, cpu)
-	if end {
-		time.Sleep(time.Duration(duration) * time.Second)
-		DeleteSliceFromCoreNetwork(snssai)
-	} 
-}
-
-// modify resource and apply to core network (by calling the nssmf of free5gc)
-func SliceModifyBandwidth(snssai string, bandwidth int) {
-
-}
-
-// get slice requests in the same time_window (by reading the slice-requests/timewindow.yaml)
-func RefreshRequestList(timewindowID int, forecastingFinish bool) ([]Slice){
-	return yamlparse.RefreshRequestList(timewindowID, forecastingFinish)
-}
-
-// schedule network slices in the same time_window
-func SlicesScheduler(slicesDeploy []SliceDeploy) {
-	for i :=0; i< len(slicesDeploy); i++ {
-		slice_name  := strings.Split(slicesDeploy[i].Name, "-")
-		gnb_ip      := gnb_ip_dictionary[slicesDeploy[i].Ngci]
-		gnb_n3_ip_B := gnb_ip_B_dictionary[slicesDeploy[i].Ngci]
-		ngci        := slicesDeploy[i].Ngci
-		start       := slicesDeploy[i].Start
-		duration    := slicesDeploy[i].Duration
-		end         := slicesDeploy[i].End
-		cpu         := slicesDeploy[i].Resource
-
-		// modify existed network slice or not
-		if len(slice_name) > 1 && slice_name[1] != "1" {
-			go SliceModifyServiceCPU(slice_name[0], ngci, cpu, start, duration, end)
-		} else {
-			go ApplySliceToCoreNetwork(slice_name[0], gnb_ip, gnb_n3_ip_B, ngci, cpu, CPUofUserPlane, start, duration, end)
-		}
-	}
-}
-
-// example
-// ApplySliceToCoreNetwork("0x01010203", "192.168.72.50", "200", "466-01-000000010", 600, CPUofUserPlane)
-// DeleteSliceFromCoreNetwork("0x01010203")
-// kubectl -n free5gc set resources deployment service-466-93-000000010-0x01030203 --limits cpu=200m --requests cpu=200m
-
+// change test case of network slice request and choose algorithm of network slice bin packing
+// var slcieRequestCase = "demo"
+var slcieRequestCase = "test-10"
+// var algorithm = "right-top"
+// var algorithm = "top-right"
+var algorithm = "leaf-size"
+//var timeWindowNumber = nsrhandler.GetTestCaseTimewindowNumber(slcieRequestCase)
+var forecastingTime = 0
+var forecastingFinish = false
 
 func main() {
-	var request []Slice
-	var bin Bin
-	var access []Slice
-	var reject []Slice
-	var deploy_info []SliceDeploy
-	var p Packer
+	// gnb.GetgNBinfo()
+	// fmt.Println("Restart gNB ...")
+	// gnb.RestartgNB("466-01-000000010")
+	// gnb.RestartgNB("466-11-000000010")
+	// gnb.RestartgNB("466-93-000000010")
+	// // warm up 2 mins
+	// warnup := nsrhandler.GetSliceInfo(slcieRequestCase)
+	// for i := 0; i < len(warnup); i++ {
+	// 	slice_name  := warnup[i].Name
+	// 	gnb_ip      := gnb_ip_dictionary[warnup[i].Ngci]
+	// 	gnb_n3_ip_B := gnb_ip_B_dictionary[warnup[i].Ngci]
+	// 	ngci        := warnup[i].Ngci
+	// 	cpu         := warnup[i].Height
 
-	// ApplySliceToCoreNetwork("0x01010203", "192.168.72.51", "201", "466-01-000000010", 600, CPUofUserPlane,1,1)
-	// ApplySliceToCoreNetwork("0x01020203", "192.168.72.53", "202", "466-11-000000010", 600, CPUofUserPlane,1,1)
-	// ApplySliceToCoreNetwork("0x01030203", "192.168.72.55", "203", "466-93-000000010", 600, CPUofUserPlane,1,1)
+	// 	// deploy to kuberenets with replicca 0
+	// 	f5gnssmf.DeploySliceToCoreNetwork(slice_name, gnb_ip, gnb_n3_ip_B, ngci, cpu, CPUofUserPlane)
+	// 	f5gnssmf.ServiceWarmUp(slice_name, ngci)
+	// }
+	// fmt.Println("Warm up time ...")
+	// time.Sleep(30 * time.Second)
 
-	// SliceModifyServiceCPU("0x01030203", "466-93-000000010", 400)
+	// accept_count := 0
+	// reject_count := 0
+	// slicebinpack.Mkdir(slcieRequestCase)
 
-	// DeleteSliceFromCoreNetwork("0x01010203")
-	// DeleteSliceFromCoreNetwork("0x01020203")
-	// DeleteSliceFromCoreNetwork("0x01030203")
-	// nssmfyamlparse.ModifyCPU("0x01010203", "upf", 300)
+	// for i := 0; i < timeWindowNumber; i++ {
+	// 	count := i + 1
 
-    // fmt.Println("123")
+	// 	if count == forecastingTime {
+	// 		forecastingFinish = true
+	// 	}
 
-	// // stop := make(chan bool)
-	// // ticker := time.NewTicker(60 * time.Second)
-	timeWindowNumber := 2
-	forecastingTime := 1
-	forecastingFinish := false
+	// 	dt := time.Now()
+	// 	fmt.Println("")
+	// 	fmt.Println(dt.String())
+	// 	fmt.Println("")
+	// 	fmt.Println("Read Network Slice Requests ", count)
+	// 	fmt.Println("")
 
-	for i := 0; i < timeWindowNumber; i++ {
-		count := i + 1
-		dt := time.Now()
-		fmt.Println(dt.String())
-		fmt.Println("")
-		fmt.Println("Read Network Slice Requests ", count)
-		fmt.Println("")
-		request = yamlparse.RefreshRequestList(count, forecastingFinish)
-		fmt.Println(request)
+	// 	// get network slice request by test case, and generate ue request pattern by network slice request
+	// 	request, ueGenerator = nsrhandler.RefreshRequestList(slcieRequestCase, count, forecastingFinish)
+	// 	fmt.Println(request)
+	// 	fmt.Println(ueGenerator)
+	// 	fmt.Println("")
+	// 	fmt.Println("Result of Network Slice Bin Packing")
+	// 	fmt.Println("")
 
-		fmt.Println("")
-		fmt.Println("Result of Network Slice Bin Packing")
-		fmt.Println("")
-		bin = Bin{"Resource", TimeWindowSize, ResourceLimit, request}
-		p = Packer{bin, access, reject, deploy_info}
-		p.Pack()
+	// 	bin = Bin{"Resource", TimeWindowSize, ResourceLimit, request}
+	// 	p = Packer{bin, access, reject, deploy_info, draw_info}
+	// 	p.Pack(algorithm)
 
-		fmt.Println("Accept Slices: ", p.AcceptSlices)
-		fmt.Println("Reject Slices: ", p.RejectSlices)
-		fmt.Println("Deploy Info:   ", p.DeployInfos)
+	// 	fmt.Println("Accept Slices: ", p.AcceptSlices)
+	// 	fmt.Println("Reject Slices: ", p.RejectSlices)
+	// 	fmt.Println("Deploy Info:   ", p.DeployInfos)
+	// 	fmt.Println("Draw Info:     ", p.DrawInfos)
+	// 	fmt.Println("")
 
-		SlicesScheduler(p.DeployInfos)
+	// 	accept_count += len(p.AcceptSlices)
+	// 	reject_count += len(p.RejectSlices)
+	// 	// scheduler.SlicesScheduler(p.DeployInfos, gnb_ip_dictionary, gnb_ip_B_dictionary, DeployTimeBias, CPUofUserPlane, ueGenerator, RequestPattern)
+	// 	slicebinpack.DrawBinPackResult(slcieRequestCase, strconv.Itoa(count), p.DrawInfos, TimeWindowSize, ResourceLimit, DrawScaleRatio)
 
+	// 	// time.Sleep(time.Duration(TimeWindowSize + TimeWindowDelay) * time.Second)
+	// }
+	
+	// for i := 0; i < len(warnup); i++ {
+	// 	slice_name  := warnup[i].Name
+	// 	// delete from core network
+	// 	f5gnssmf.DeleteSliceFromCoreNetwork(slice_name)
+	// }
 
-		if count == forecastingTime {
-			forecastingFinish = true
-		}
-		time.Sleep(720 * time.Second)
-	}
-	//ApplySliceToCoreNetwork("0x01010203", "50", "200", "466-01-000000010", 600)
+	// fmt.Println("Accept count: ", accept_count)
+	// fmt.Println("Reject count: ", reject_count)
+	// fmt.Println("Accept rate : ", float64(accept_count) / 4000.0)
 
-
-
-	//DeleteSliceFromCoreNetwork("0x01010203")
+	// gnb.RestartgNB("466-01-000000010")
+	// gnb.RestartgNB("466-11-000000010")
+	// gnb.RestartgNB("466-93-000000010")
+	//nssmf.ApplyNetworkSlice("0x01010203", "192.168.72.51", "200", "466-01-000000010", 600, 200)
+	// nssmf.DeployNetworkSlice("0x01010203", "192.168.72.51", "200", "466-01-000000010", 600, 200)
+	// nssmf.ApplyNetworkSlice("0x01010203", "466-01-000000010")
+	nssmf.DeleteNetworkSlice("0x01010203")
+	//f5gnssmf.ApplySliceToCoreNetwork("0x01010203", "192.168.72.51", "200", "466-01-000000010", 600, 200, 0, 60, true, 1.04)
 }
+
+// test example:
+
+// apply specific network slice
+// f5gnssmf.ApplySliceToCoreNetwork("0x01010203", "192.168.72.51", "201", "466-01-000000010", 600, 200, 0, 60, true, 1.04)
+// f5gnssmf.DeleteSliceFromCoreNetwork("0x01010203")
+// nssmf.ApplyNetworkSlice("0x01010203", "192.168.72.51", "201", "466-01-000000010", 600, 200)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/p76081158/5g-nsmf
 go 1.14
 
 require (
-	github.com/p76081158/free5gc-nssmf v0.0.0-20210525030650-9afb78e6669d
+	github.com/Arafatk/glot v0.0.0-20180312013246-79d5219000f0
+	github.com/p76081158/free5gc-nssmf v0.0.0-20210616172500-b20fa150920c
 	gopkg.in/yaml.v2 v2.4.0
 )

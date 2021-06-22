a3fe6ee9198edbcd0fe78e8c4caf873bce2aaf8b
p76081158 <p76081158@gs.ncku.edu.tw>
2021-06-23 03:03:54 +0800

    initial version of creating dataset

--- nsrtester/go.mod
+++ nsrtester/go.mod
@@ -1,3 +1,9 @@
 module github.com/p76081158/5g-nsmf/nsrtester
 
 go 1.14
+
+require (
+	github.com/p76081158/5g-nsmf/nsrgenerator v0.0.0-20210622140012-2a9470436021
+	github.com/p76081158/free5gc-nssmf v0.0.0-20210616172500-b20fa150920c // indirect
+	gopkg.in/yaml.v2 v2.4.0
+)
--- nsrtester/tester.go
+++ nsrtester/tester.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
+	"os"
 	// "time"
 	// "strconv"
 
@@ -12,15 +13,40 @@ import (
 	// "github.com/p76081158/5g-nsmf/modules/ueransim/ue/generator"
 	// "github.com/p76081158/5g-nsmf/api/f5gnssmf"
 	// "github.com/p76081158/free5gc-nssmf"
-	"github.com/p76081158/5g-nsmf/nsrtest/modules/yamltopara"
+	"github.com/p76081158/5g-nsmf/nsrtester/modules/nsrcreater"
+	"github.com/p76081158/5g-nsmf/nsrtester/modules/yamltopara"
 )
 
 type DatasetInfo = yamltopara.DatasetInfo
 type Resource = yamltopara.Resource
 
+var Dir = "test-default"
 var slcieRequestCase = "test-10"
-var algorithm = "right-top"
-// var timeWindowNumber = nsrhandler.GetTestCaseTimewindowNumber(slcieRequestCase)
+var algorithm_list = []string {"right-top", "top-right", "leaf-size"}
+var gnb_tenant_dictionary = []string {"466-01-000000010", "466-11-000000010", "466-93-000000010"}
+
+var Tenant = 3
+var SliceNum = 10
+var ExtraRequestNum = 1
+var TestNum = 5
+
+var CpuLimit = 1000
+var TimeWindowSize = 600
+var ForecastBlockSize = 150
+var Cpu_base = 100
+var Cpu_min = 2
+var Cpu_max = CpuLimit / Cpu_base
+var Cpu_half = CpuLimit / (Cpu_base * 2)
+var Cpu_lambda = Cpu_half
+var Cpu_discount = 0.5
+
+var BandwidthLimit = 10
+var Bandwidth_min = 1
+var Bandwidth_lambda = 5
+var Bandwidth_discount = 0.5
+
+var Slice_min_accept_num = 2
+var Slice_duration = TimeWindowSize / Slice_min_accept_num
 
 func main() {
 	if len(os.Args) != 2 {
@@ -30,6 +56,27 @@ func main() {
 	if (os.Args[1]!="") {
 		Dir = string(os.Args[1])
 	}
+	// get parameter from yaml file
 	info := yamltopara.GetDataSetInfo(Dir)
-	fmt.Println(info.Resource)
+	gnb_tenant_dictionary = info.NgciList
+	Tenant = len(gnb_tenant_dictionary)
+	SliceNum = info.SliceNum
+	ExtraRequestNum = info.ExtraRequest
+	TestNum = info.TestNum
+	// cpu parameter
+	CpuLimit = info.Resource.Cpu.Limit / Cpu_base
+	ForecastBlockSize = info.ForecastBlockSize
+	Cpu_lambda = info.Resource.Cpu.Lambda / Cpu_base
+	Cpu_discount = info.Resource.Cpu.Discount
+	// bandwidth parameter
+	BandwidthLimit = info.Resource.Bandwidth.Limit
+	Bandwidth_lambda = info.Resource.Bandwidth.Lambda
+	Bandwidth_discount = info.Resource.Bandwidth.Discount
+	// slice and timewindow info
+	Slice_duration = info.Resource.Duration
+	TimeWindowSize = info.Timewindow
+	ForecastBlockSize = info.ForecastBlockSize
+	fmt.Println(gnb_tenant_dictionary)
+	// create dataset by parameter in yaml file
+	nsrcreater.CreateDataSet(Dir, TestNum, gnb_tenant_dictionary, SliceNum, CpuLimit, Cpu_lambda, BandwidthLimit, Bandwidth_lambda, Slice_duration, ExtraRequestNum, ForecastBlockSize, Cpu_discount, Bandwidth_discount)
 }

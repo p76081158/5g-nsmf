ba0d9321dec22e761972dc8584a913a1ce3f31cc
p76081158 <p76081158@gs.ncku.edu.tw>
2021-07-01 17:10:33 +0800

    implement sorting interface for node

--- modules/nsrhandler/request.go
+++ modules/nsrhandler/request.go
@@ -23,12 +23,12 @@ type SliceList struct {
 type ByCpu []SliceList
 
 func (a ByCpu) Len() int           { return len(a) }
-func (a ByCpu) Less(i, j int) bool { return a[i].Cpu > a[j].Cpu }
+func (a ByCpu) Less(i, j int) bool { return a[i].Cpu > a[j].Cpu }                     // descending sort
 func (a ByCpu) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 // ByCpu implements sort.Interface based on the Cpu field.
 type ByBandwidth []SliceList
 
 func (a ByBandwidth) Len() int           { return len(a) }
-func (a ByBandwidth) Less(i, j int) bool { return a[i].Bandwidth > a[j].Bandwidth }
+func (a ByBandwidth) Less(i, j int) bool { return a[i].Bandwidth > a[j].Bandwidth }   // descending sort
 func (a ByBandwidth) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }

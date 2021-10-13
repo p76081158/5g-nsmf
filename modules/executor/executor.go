02662011bc38ee8ea58ce76d0fb878cec40db9d9
Vcx <p76081158@gs.ncku.edu.tw>
2021-10-13 23:03:02 +0800

    add run-demo and debug script

--- modules/executor/executor.go
+++ modules/executor/executor.go
@@ -14,6 +14,9 @@ import (
 )
 
 func RunDemo() {
+	var slcieRequestCase  = "demo"
+	var algorithm         = "invert-pre-order"
+	var timeWindowNumber  = nsrhandler.GetTestCaseTimewindowNumber( "slice-requests/" + slcieRequestCase)
 	// gnb.GetgNBinfo()
 	// fmt.Println("Restart gNB ...")
 	// gnb.RestartgNB("466-01-000000010")
@@ -23,7 +26,8 @@ func RunDemo() {
 	GetgNBinfo()
 	fmt.Println("Restart gNB ...")
 	RestartAllgNB()
-	
+	time.Sleep(60 * time.Second)
+
 	// // warm up 2 mins
 	warnup := nsrhandler.GetSliceInfo(slcieRequestCase)
 	for i := 0; i < len(warnup); i++ {
@@ -67,6 +71,7 @@ func RunDemo() {
 		fmt.Println("Result of Network Slice Bin Packing")
 		fmt.Println("")
 
+		// *** can change bin size here ***
 		bin = Bin{"Resource", TimeWindowSize, ResourceLimit, requestCpu}
 		p = Packer{bin, access, reject, deploy_info, draw_info}
 		p.Pack(algorithm, concat)
@@ -94,4 +99,101 @@ func RunDemo() {
 	fmt.Println("Accept count: ", accept_count)
 	fmt.Println("Reject count: ", reject_count)
 	fmt.Println("Accept rate : ", float64(accept_count) / 4000.0)
+}
+
+func RunAlgorithmTest() {
+	// var slcieRequestCase  = "demo"
+	var slcieRequestCase  = "multi-tenant/466-01-000000010"
+	var algorithm         = "invert-pre-order"
+	// var algorithm         = "node-concat"
+	var timeWindowNumber  = nsrhandler.GetTestCaseTimewindowNumber( "slice-requests/" + slcieRequestCase)
+	// gnb.GetgNBinfo()
+	// fmt.Println("Restart gNB ...")
+	// gnb.RestartgNB("466-01-000000010")
+	// gnb.RestartgNB("466-11-000000010")
+	// gnb.RestartgNB("466-93-000000010")
+
+//	GetgNBinfo()
+//	fmt.Println("Restart gNB ...")
+//	RestartAllgNB()
+	
+	// // warm up 2 mins
+	// warnup := nsrhandler.GetSliceInfo(slcieRequestCase)
+	// for i := 0; i < len(warnup); i++ {
+	// 	slice_name  := warnup[i].Snssai
+	// 	gnb_ip      := gnb_ip_dictionary[warnup[i].Ngci]
+	// 	gnb_n3_ip_B := gnb_ip_B_dictionary[warnup[i].Ngci]
+	// 	ngci        := warnup[i].Ngci
+	// 	cpu         := warnup[i].Cpu
+
+	// 	// deploy to kuberenets with replicca 0
+	// 	f5gnssmf.DeploySliceToCoreNetwork(slice_name, gnb_ip, gnb_n3_ip_B, ngci, cpu, CPUofUserPlane)
+	// 	f5gnssmf.ServiceWarmUp(slice_name, ngci)
+	// }
+	// fmt.Println("Warm up time ...")
+	// time.Sleep(30 * time.Second)
+
+	accept_count := 0
+	reject_count := 0
+	slicebinpack.Mkdir("logs/binpack/" + slcieRequestCase)
+
+	for i := 0; i < timeWindowNumber; i++ {
+		count := i + 1
+
+		if count == forecastingTime {
+			forecastingFinish = true
+		}
+
+		dt := time.Now()
+		fmt.Println("")
+		fmt.Println(dt.String())
+		fmt.Println("")
+		fmt.Println("Read Network Slice Requests ", count)
+		fmt.Println("")
+
+		// get network slice request by test case, and generate ue request pattern by network slice request
+		requestCpu, requestBandwidth, ueGenerator = nsrhandler.RefreshRequestList("slice-requests/" + slcieRequestCase, "slice-forecasting/" + slcieRequestCase, count, forecastingFinish, sort)
+		fmt.Println(requestCpu)
+		fmt.Println(requestBandwidth)
+		fmt.Println(ueGenerator)
+		fmt.Println("")
+		fmt.Println("Result of Network Slice Bin Packing")
+		fmt.Println("")
+
+		bin = Bin{"Resource", TimeWindowSize, ResourceLimit, requestCpu}
+		p = Packer{bin, access, reject, deploy_info, draw_info}
+		p.Pack(algorithm, concat)
+
+		fmt.Println("Accept Slices: ", p.AcceptSlices)
+		fmt.Println("Reject Slices: ", p.RejectSlices)
+		fmt.Println("Deploy Info:   ", p.DeployInfos)
+		fmt.Println("Draw Info:     ", p.DrawInfos)
+		fmt.Println("")
+
+		accept_count += len(p.AcceptSlices)
+		reject_count += len(p.RejectSlices)
+//		scheduler.SlicesScheduler(p.DeployInfos, gnb_ip_dictionary, gnb_ip_B_dictionary, DeployTimeBias, CPUofUserPlane, ueGenerator, RequestPattern)
+		slicebinpack.DrawBinPackResult("logs/binpack/" + slcieRequestCase, strconv.Itoa(count), p.DrawInfos, TimeWindowSize, ResourceLimit, DrawScaleRatio)
+
+		// time.Sleep(time.Duration(TimeWindowSize + TimeWindowDelay) * time.Second)
+	}
+	
+	// for i := 0; i < len(warnup); i++ {
+	// 	slice_name  := warnup[i].Snssai
+	// 	// delete from core network
+	// 	f5gnssmf.DeleteSliceFromCoreNetwork(slice_name)
+	// }
+
+	fmt.Println("Accept count: ", accept_count)
+	fmt.Println("Reject count: ", reject_count)
+	fmt.Println("Accept rate : ", float64(accept_count) / 4000.0)
+}
+
+
+// for debug
+func RunDebug() {
+	// f5gnssmf.ApplySliceToCoreNetwork("0x01010203", "192.168.72.51", "200", "466-01-000000010", 600, 200, 0, 60, true, 1.04)
+	f5gnssmf.DeleteSliceFromCoreNetwork("0x01010203")
+	f5gnssmf.DeleteSliceFromCoreNetwork("0x01020203")
+	f5gnssmf.DeleteSliceFromCoreNetwork("0x01030203")
 }

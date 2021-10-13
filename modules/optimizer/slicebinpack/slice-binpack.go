d0a167c02da7b69ee3ae302a59f720997540a5fe
Vcx <p76081158@gs.ncku.edu.tw>
2021-10-13 23:10:12 +0800

    add new algorithm node-concat

--- modules/optimizer/slicebinpack/slice-binpack.go
+++ modules/optimizer/slicebinpack/slice-binpack.go
@@ -63,7 +63,7 @@ func (p *Packer) Pack(algorithm string, concat bool) {
 		var w, h int
 		sub_slices_num := len(p.Bins.Slices[i].SubBlock)
 
-		// choose which node for putting the slice (could be multiple blocks)
+		// choose which height for putting the slice (could be multiple blocks)
 		// w, h = 0, 0
 		h = 0
 		w = p.Bins.Slices[i].Width
@@ -80,27 +80,70 @@ func (p *Packer) Pack(algorithm string, concat bool) {
 		}
 
 		// select algorithm for finding candidate of network slice placement
-		node := root.find(w, h, algorithm, tree_list)
-		if node != nil {
-			// is slice group or not
-			if sub_slices_num > 0 {
-				for j := 0; j < sub_slices_num; j++ {
-					w = p.Bins.Slices[i].SubBlock[j].Width
-                    h = p.Bins.Slices[i].SubBlock[j].Height
+		if algorithm == "node-concat" {
+			append_all, append_top, append_deploy, append_draw := concatTop(tree_list, tree_list, p.Bins.Slices[i])
+			if append_all != nil {
+				tree_list      = append(tree_list, append_all...)
+				tree_list_top  = append(tree_list_top, append_top...)
+				p.AcceptSlices = append(p.AcceptSlices, p.Bins.Slices[i])
+				p.DeployInfos  = append(p.DeployInfos, append_deploy...)
+				p.DrawInfos    = append(p.DrawInfos, append_draw...)
+			} else {
+				p.RejectSlices = append(p.RejectSlices, p.Bins.Slices[i])
+			}
+			continue
+		} else {
+			node := root.find(w, h, algorithm, tree_list)
+			if node != nil {
+				// is slice group or not
+				if sub_slices_num > 0 {
+					for j := 0; j < sub_slices_num; j++ {
+						w    = p.Bins.Slices[i].SubBlock[j].Width
+						h    = p.Bins.Slices[i].SubBlock[j].Height
+						node = node.split(w, h)
+						updateTree(node, tree_list, w, h)
+	
+						tree_list       = append(tree_list, node.top)
+						tree_list       = append(tree_list, node.right)
+						tree_list_top   = append(tree_list_top, node.top)
+						tree_list_right = append(tree_list_right, node.right)
+						end  := false
+						info := SliceDeploy {
+							Name:     p.Bins.Slices[i].SubBlock[j].Name,
+							Ngci:     p.Bins.Slices[i].Ngci,
+							Start:    node.x,
+							Duration: w,
+							End:      end,
+							Resource: h,
+						}
+						drawinfo := DrawBlock {
+							TopLeftX:   node.x,
+							TopLeftY:   node.y + h,
+							DownRightX: node.x + w,
+							DownRightY: node.y,
+						}
+						if j == sub_slices_num - 1 {
+							info.End = true
+						} else {
+							node = node.right
+						}
+						p.DeployInfos = append(p.DeployInfos, info)
+						p.DrawInfos   = append(p.DrawInfos, drawinfo)
+					}
+					p.AcceptSlices    = append(p.AcceptSlices, p.Bins.Slices[i])
+				} else {
 					node = node.split(w, h)
 					updateTree(node, tree_list, w, h)
-
-					tree_list       = append(tree_list, node.top)
-					tree_list       = append(tree_list, node.right)
-					tree_list_top   = append(tree_list_top, node.top)
-					tree_list_right = append(tree_list_right, node.right)
-					end := false
+					tree_list         = append(tree_list, node.top)
+					tree_list         = append(tree_list, node.right)
+					tree_list_top     = append(tree_list_top, node.top)
+					tree_list_right   = append(tree_list_right, node.right)
 					info := SliceDeploy {
-						Name:     p.Bins.Slices[i].SubBlock[j].Name,
+						Name:     p.Bins.Slices[i].Name,
 						Ngci:     p.Bins.Slices[i].Ngci,
 						Start:    node.x,
 						Duration: w,
-						End:      end,
+						End:      true,
 						Resource: h,
 					}
 					drawinfo := DrawBlock {
@@ -109,55 +152,106 @@ func (p *Packer) Pack(algorithm string, concat bool) {
 						DownRightX: node.x + w,
 						DownRightY: node.y,
 					}
-					if j == sub_slices_num - 1 {
-						info.End = true
-					} else {
-						node = node.right
-					}
-					p.DeployInfos = append(p.DeployInfos, info)
-					p.DrawInfos   = append(p.DrawInfos, drawinfo)
-				}
-				p.AcceptSlices    = append(p.AcceptSlices, p.Bins.Slices[i])
-			} else {
-				node = node.split(w, h)
-				updateTree(node, tree_list, w, h)
-				tree_list         = append(tree_list, node.top)
-				tree_list         = append(tree_list, node.right)
-				tree_list_top     = append(tree_list_top, node.top)
-				tree_list_right   = append(tree_list_right, node.right)
-				info := SliceDeploy {
-					Name:     p.Bins.Slices[i].Name,
-					Ngci:     p.Bins.Slices[i].Ngci,
-					Start:    node.x,
-					Duration: w,
-					End:      true,
-					Resource: h,
-				}
-				drawinfo := DrawBlock {
-					TopLeftX:   node.x,
-					TopLeftY:   node.y + h,
-					DownRightX: node.x + w,
-					DownRightY: node.y,
-				}
-			    p.AcceptSlices = append(p.AcceptSlices, p.Bins.Slices[i])
-				p.DeployInfos  = append(p.DeployInfos, info)
-				p.DrawInfos    = append(p.DrawInfos, drawinfo)
-			}
-		} else {
-			if concat {
-				append_all, append_top, append_deploy, append_draw := concatTop(tree_list, tree_list_top, p.Bins.Slices[i])
-				if append_all != nil {
-					tree_list      = append(tree_list, append_all...)
-					tree_list_top  = append(tree_list_top, append_top...)
 					p.AcceptSlices = append(p.AcceptSlices, p.Bins.Slices[i])
-					p.DeployInfos  = append(p.DeployInfos, append_deploy...)
-					p.DrawInfos    = append(p.DrawInfos, append_draw...)
-				} else {
-					p.RejectSlices = append(p.RejectSlices, p.Bins.Slices[i])
+					p.DeployInfos  = append(p.DeployInfos, info)
+					p.DrawInfos    = append(p.DrawInfos, drawinfo)
 				}
 			} else {
-				p.RejectSlices     = append(p.RejectSlices, p.Bins.Slices[i])
+				// when can't find node to fit network slice request, using tree node concatenation algorithm
+				if concat {
+					append_all, append_top, append_deploy, append_draw := concatTop(tree_list, tree_list_top, p.Bins.Slices[i])
+					if append_all != nil {
+						tree_list      = append(tree_list, append_all...)
+						tree_list_top  = append(tree_list_top, append_top...)
+						p.AcceptSlices = append(p.AcceptSlices, p.Bins.Slices[i])
+						p.DeployInfos  = append(p.DeployInfos, append_deploy...)
+						p.DrawInfos    = append(p.DrawInfos, append_draw...)
+					} else {
+						p.RejectSlices = append(p.RejectSlices, p.Bins.Slices[i])
+					}
+				} else {
+					p.RejectSlices     = append(p.RejectSlices, p.Bins.Slices[i])
+				}
 			}
 		}
+		
+		// if node != nil {
+		// 	// is slice group or not
+		// 	if sub_slices_num > 0 {
+		// 		for j := 0; j < sub_slices_num; j++ {
+		// 			w = p.Bins.Slices[i].SubBlock[j].Width
+        //             h = p.Bins.Slices[i].SubBlock[j].Height
+		// 			node = node.split(w, h)
+		// 			updateTree(node, tree_list, w, h)
+
+		// 			tree_list       = append(tree_list, node.top)
+		// 			tree_list       = append(tree_list, node.right)
+		// 			tree_list_top   = append(tree_list_top, node.top)
+		// 			tree_list_right = append(tree_list_right, node.right)
+		// 			end := false
+		// 			info := SliceDeploy {
+		// 				Name:     p.Bins.Slices[i].SubBlock[j].Name,
+		// 				Ngci:     p.Bins.Slices[i].Ngci,
+		// 				Start:    node.x,
+		// 				Duration: w,
+		// 				End:      end,
+		// 				Resource: h,
+		// 			}
+		// 			drawinfo := DrawBlock {
+		// 				TopLeftX:   node.x,
+		// 				TopLeftY:   node.y + h,
+		// 				DownRightX: node.x + w,
+		// 				DownRightY: node.y,
+		// 			}
+		// 			if j == sub_slices_num - 1 {
+		// 				info.End = true
+		// 			} else {
+		// 				node = node.right
+		// 			}
+		// 			p.DeployInfos = append(p.DeployInfos, info)
+		// 			p.DrawInfos   = append(p.DrawInfos, drawinfo)
+		// 		}
+		// 		p.AcceptSlices    = append(p.AcceptSlices, p.Bins.Slices[i])
+		// 	} else {
+		// 		node = node.split(w, h)
+		// 		updateTree(node, tree_list, w, h)
+		// 		tree_list         = append(tree_list, node.top)
+		// 		tree_list         = append(tree_list, node.right)
+		// 		tree_list_top     = append(tree_list_top, node.top)
+		// 		tree_list_right   = append(tree_list_right, node.right)
+		// 		info := SliceDeploy {
+		// 			Name:     p.Bins.Slices[i].Name,
+		// 			Ngci:     p.Bins.Slices[i].Ngci,
+		// 			Start:    node.x,
+		// 			Duration: w,
+		// 			End:      true,
+		// 			Resource: h,
+		// 		}
+		// 		drawinfo := DrawBlock {
+		// 			TopLeftX:   node.x,
+		// 			TopLeftY:   node.y + h,
+		// 			DownRightX: node.x + w,
+		// 			DownRightY: node.y,
+		// 		}
+		// 	    p.AcceptSlices = append(p.AcceptSlices, p.Bins.Slices[i])
+		// 		p.DeployInfos  = append(p.DeployInfos, info)
+		// 		p.DrawInfos    = append(p.DrawInfos, drawinfo)
+		// 	}
+		// } else {
+		// 	if concat {
+		// 		append_all, append_top, append_deploy, append_draw := concatTop(tree_list, tree_list_top, p.Bins.Slices[i])
+		// 		if append_all != nil {
+		// 			tree_list      = append(tree_list, append_all...)
+		// 			tree_list_top  = append(tree_list_top, append_top...)
+		// 			p.AcceptSlices = append(p.AcceptSlices, p.Bins.Slices[i])
+		// 			p.DeployInfos  = append(p.DeployInfos, append_deploy...)
+		// 			p.DrawInfos    = append(p.DrawInfos, append_draw...)
+		// 		} else {
+		// 			p.RejectSlices = append(p.RejectSlices, p.Bins.Slices[i])
+		// 		}
+		// 	} else {
+		// 		p.RejectSlices     = append(p.RejectSlices, p.Bins.Slices[i])
+		// 	}
+		// }
 	}
 }

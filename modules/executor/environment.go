f3e53a0f453da78d4dca13990868439325beb32b
Vcx <p76081158@gs.ncku.edu.tw>
2021-10-17 21:29:23 +0800

    update parameter

--- modules/executor/environment.go
+++ modules/executor/environment.go
@@ -1,7 +1,7 @@
 package executor
 
 import (
-	"github.com/p76081158/5g-nsmf/modules/nsrhandler"
+	//"github.com/p76081158/5g-nsmf/modules/nsrhandler"
 	"github.com/p76081158/5g-nsmf/modules/optimizer/slicebinpack"
 	"github.com/p76081158/5g-nsmf/modules/ueransim/ue/generator"
 )
@@ -37,17 +37,20 @@ var DeployTimeBias      = 1.04
 var DrawScaleRatio      = 10
 var gnb_ip_dictionary   = map[string]string{"466-01-000000010": "192.168.72.51", "466-11-000000010": "192.168.72.53", "466-93-000000010": "192.168.72.55"}
 var gnb_ip_B_dictionary = map[string]string{"466-01-000000010": "200", "466-11-000000010": "201", "466-93-000000010": "202"}
-var RequestPattern      = "300"
+// var RequestPattern      = "300"
+var RequestPattern      = "1200"
 
 // change test case of network slice request and choose algorithm of network slice bin packing
-var slcieRequestCase  = "demo"
+// var slcieRequestCase  = "demo"
 // var slcieRequestCase = "demo-1"
 // var slcieRequestCase = "DataSet-4/test1"
-var algorithm         = "invert-pre-order"
+// var algorithm         = "invert-pre-order"
 // var algorithm         = "pre-order"
 // var algorithm         = "leaf-size"
-var timeWindowNumber  = nsrhandler.GetTestCaseTimewindowNumber( "slice-requests/" + slcieRequestCase)
-var forecastingTime   = 1
+// var timeWindowNumber  = nsrhandler.GetTestCaseTimewindowNumber( "slice-requests/" + slcieRequestCase)
+
+// using forecasted data at which timewindow ( 0 == never, 1 == timewindow-1, ... )
+var forecastingTime   = 2
 var forecastingFinish = false
 // var sort              = true
 var sort              = false

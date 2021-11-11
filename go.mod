a28ee16bf6e917fcbd30322374c8f6b596e1a3d0
Vcx <p76081158@gs.ncku.edu.tw>
2021-11-10 23:04:09 -0800

    add network slice reqeust's label to the img of logs

--- go.mod
+++ go.mod
@@ -5,17 +5,18 @@ go 1.14
 require (
 	github.com/Arafatk/glot v0.0.0-20180312013246-79d5219000f0
 	github.com/p76081158/free5gc-nssmf v0.0.0-20210616172500-b20fa150920c
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 replace (
+	github.com/p76081158/5g-nsmf/api/f5gnssmf => ./api/f5gnssmf
 	github.com/p76081158/5g-nsmf/modules/executor => ./modules/executor
 	github.com/p76081158/5g-nsmf/modules/logger => ./modules/logger
 	github.com/p76081158/5g-nsmf/modules/nsrhandler => ./modules/nsrhandler
+	github.com/p76081158/5g-nsmf/modules/optimizer/scheduler => ./modules/optimizer/scheduler
 	github.com/p76081158/5g-nsmf/modules/optimizer/slicebinpack => ./modules/optimizer/slicebinpack
 	github.com/p76081158/5g-nsmf/modules/optimizer/tenantbinpack => ./modules/optimizer/tenantbinpack
-	github.com/p76081158/5g-nsmf/modules/optimizer/scheduler => ./modules/optimizer/scheduler
 	github.com/p76081158/5g-nsmf/modules/ueransim/gnb => ./modules/ueransim/gnb
 	github.com/p76081158/5g-nsmf/modules/ueransim/ue/generator => ./modules/ueransim/ue/generator
-	github.com/p76081158/5g-nsmf/api/f5gnssmf => ./api/f5gnssmf
 )
--- modules/optimizer/slicebinpack/draw.go
+++ modules/optimizer/slicebinpack/draw.go
@@ -3,6 +3,7 @@ package slicebinpack
 import (
 	"fmt"
 	"time"
+	"strconv"
 	"image"
 	"image/color"
 	"image/draw"
@@ -11,6 +12,10 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
 )
 
 // create dir for new set of network slice requests
@@ -24,23 +29,41 @@ func Mkdir(dir string) {
 	fmt.Println("create dir for bin packing picture : logs/binapck/", dir)
 }
 
+// add label to Network Slice Requests
+func addLabel(img *image.RGBA, x, y int, label string) {
+	col := color.RGBA{255, 255, 255, 255}
+	point := fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)}
+
+	d := &font.Drawer{
+		Dst:  img,
+		Src:  image.NewUniform(col),
+		Face: basicfont.Face7x13,
+		Dot:  point,
+	}
+	d.DrawString(label)
+}
+
 // draw deployment of network slice requests
 func DrawBinPackResult(dir string, id string, drawinfos []DrawBlock, width int, height int, scale_ratio int) {
 	img           := image.NewRGBA(image.Rect(0, height / scale_ratio * (-1), width / scale_ratio, 0))
 	new_png_file  := dir + "/timewindow-" + id +".png"
 	for i :=0; i< len(drawinfos); i++ {
-		t         := time.Now().UnixNano()
-		r         := rand.New(rand.NewSource(t))
-		color_r   := uint8(r.Intn(255))
-		color_g   := uint8(r.Intn(255))
-		color_b   := uint8(r.Intn(255))
-		color     := color.RGBA{color_r, color_g, color_b, 255}
-		topleftx  := drawinfos[i].TopLeftX / scale_ratio
-		toplefty  := drawinfos[i].TopLeftY * (-1) / scale_ratio
-		dowrightx := drawinfos[i].DownRightX / scale_ratio
-		dowrighty := drawinfos[i].DownRightY * (-1) / scale_ratio
-		rectangle := image.Rect(topleftx, toplefty, dowrightx, dowrighty)
+		t          := time.Now().UnixNano()
+		r          := rand.New(rand.NewSource(t))
+		color_r    := uint8(r.Intn(255))
+		color_g    := uint8(r.Intn(255))
+		color_b    := uint8(r.Intn(255))
+		color      := color.RGBA{color_r, color_g, color_b, 255}
+		topleftx   := drawinfos[i].TopLeftX / scale_ratio
+		toplefty   := drawinfos[i].TopLeftY * (-1) / scale_ratio
+		downrightx := drawinfos[i].DownRightX / scale_ratio
+		downrighty := drawinfos[i].DownRightY * (-1) / scale_ratio
+		labelx     := int((topleftx + downrightx) / 2)
+		labely     := int((toplefty + downrighty) / 2)
+		label      := strconv.Itoa(i + 1)
+		rectangle  := image.Rect(topleftx, toplefty, downrightx, downrighty)
 		draw.Draw(img, rectangle, &image.Uniform{color}, image.ZP, draw.Src)
+		addLabel(img, labelx, labely, label)
 	}
     myfile, err   := os.Create(new_png_file)
     if err != nil {
